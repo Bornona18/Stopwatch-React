f2c3013151d8675bcce0c9e1d53e55b0feca33af
Bornona18 <61803232+Bornona18@users.noreply.github.com>
2020-08-06 11:44:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    class App extends Component{
 
-export default App;
+      constructor() {
+        super()
+        this.state = {
+          milliseconds: 0,
+          seconds: 0,
+          minutes: 0,
+          timeout: []
+        }
+      }
+    
+      startTime = () => {
+        this.setState({
+          timeout: this.timer()
+        })
+      }
+    
+      stopTime = () => {
+          clearInterval(this.state.timeout)
+      }
+    
+      resetTime = () => {
+          this.setState({
+            milliseconds: 0,
+            seconds: 0,
+            minutes: 0
+          })
+      }
+    
+    
+    
+      timer = () => {
+    
+        let timer = setInterval(() => {
+    
+          this.setState(state => {
+            return { milliseconds: state.milliseconds + 1}
+          });
+    
+          if (this.state.milliseconds > 9) {
+    
+            this.setState(state => {
+              return {
+                milliseconds: state.milliseconds = 0,
+                seconds: state.seconds + 1
+              }
+            })
+    
+          } 
+    
+          if (this.state.seconds > 59) {
+    
+            this.setState(state => {
+              return {
+                minutes: state.minutes + 1,
+                seconds: 0
+              }
+            })
+    
+          }
+        }, 100);
+        return timer
+    
+      }
+    
+      render() {
+        return (
+          <div className='stopwatch'>
+            Stopwatch<br/><br/>
+            {this.state.minutes} : {this.state.seconds} : {this.state.milliseconds}
+            <div className='buttons'>
+              <button onClick={this.startTime}>Start</button>
+              <button onClick={this.stopTime}>Stop</button>
+              <button onClick={this.resetTime}>Reset</button>
+            </div>
+          </div>
+        );
+      }
+    }
+
+
+    export default App;
